e5bd54e3c45a5b927134a4fe4e4b5e4ed9ebbfb6
Alexander Komlev <alexander.komlev42@gmail.com>
2024-09-07 20:08:46 +0700

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+import path from 'path';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+
+const common = {
+  mode: "development",
+  target: "web",
+  devtool: "inline-source-map",
+  // Spin up a server for quick development
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+  },
+  output: {
+    path: path.resolve(path.resolve(), 'dist'),
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
+
+export default common;
